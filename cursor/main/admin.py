b38289b092a0816b45da82ae8a31e0af9c81a2e6
BoyovEvgen <e.boyov@gmail.com>
2023-05-18 22:37:04 +0300

    adding images for slider via admin

--- cursor/main/admin.py
+++ cursor/main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import MenuItem, SliderItem
 
-# Register your models here.
+
+class SliderItemAdmin(admin.ModelAdmin):
+    exclude = ('link',)  # виключає відображення цього пункту в адмін панелі. автозаповнення прописав в models.py
+
 
 admin.site.register(MenuItem)
-admin.site.register(SliderItem)
+admin.site.register(SliderItem, SliderItemAdmin)
--- cursor/main/views.py
+++ cursor/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import MenuItem
+from .models import MenuItem, SliderItem
+from django.conf import settings
+import os
 
 # Create your views here.
 
 def main(request):
     menu_items = MenuItem.objects.all()
-    return render(request, "index.html", {"menu_items": menu_items})
+    img_urls_list = SliderItem.objects.values_list('link', flat=True)
+    context = {"menu_items": menu_items, 'img_urls': img_urls_list}
+
+    return render(request, "index.html", context)
