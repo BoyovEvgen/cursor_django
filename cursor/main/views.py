bb9ad7bf0b265c18dcaf53aff5ff9d4fbf7f41e4
BoyovEvgen <e.boyov@gmail.com>
2023-05-18 16:44:43 +0300

    start_project

---
+++ cursor/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import MenuItem
+
+# Create your views here.
+
+def main(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, "index.html", {"menu_items": menu_items})
