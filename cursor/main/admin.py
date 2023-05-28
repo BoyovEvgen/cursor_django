eec65bd3e77db47aa4097b363d4c8bfd3f46ca8f
BoyovEvgen <e.boyov@gmail.com>
2023-05-28 22:28:40 +0300

    added cart

--- cursor/main/admin.py
+++ cursor/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuItem, SliderItem
+from .models import MenuItem, SliderItem, OrderItems, Order
 
 
 class SliderItemAdmin(admin.ModelAdmin):
@@ -8,3 +8,14 @@ class SliderItemAdmin(admin.ModelAdmin):
 
 admin.site.register(MenuItem)
 admin.site.register(SliderItem, SliderItemAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = fields = ['id', 'first_name', "last_name", "address", "email"]
+
+    def queryset(self, request):
+        qs = super(Order, self).queryset(request)
+        return qs.all()
+
+
+admin.site.register(Order, OrderAdmin)
--- cursor/main/models.py
+++ cursor/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from products.models import Product
 import os
 
 # Create your models here.
@@ -27,3 +28,28 @@ class SliderItem(models.Model):
         link = os.path.join(media_path, 'uploads', file_name)  #збирає шлях, який потім rout передає в html
         self.link = link
         super().save(*args, **kwargs)
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=255)
+    total_price = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id) + " " + self.address
+
+
+class OrderItems(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return str(self.order.id) + " " + self.product.title
